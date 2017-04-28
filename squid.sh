0417831ade376662d0266a3ae0a82e335a697cd2
mundodoug <dougeproxy@gmail.com>
2017-04-28 16:29:41 -0300

    Create squid.sh

---
+++ squid.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "                                          "
+echo "******************************************"
+echo "*** Script Oficial do Canal @MundoDoug ***"
+echo "******************************************"
+echo "**************Feito por @Dougssh**********"
+echo "******************************************"
+echo "                                          "
+echo "										                    	"
+echo "                                          "
+echo "                                          "
+echo -e "\033[1;31mPara a instalação ser correta é preciso o ip.
+Digite o ip !\033[0m"
+read -p ": " ip
+clear
+echo -e "\033[1;31m-----> \033[01;37mSeu sistema operacional: Fedora\033[1;31m"
+echo -e "\033[1;31m-----> \033[01;37mSeu ip:\033[1;31m $ip"
+echo -e "\033[1;31m-----> \033[1;37mSQUID NAS PORTAS:\033[1;31m 80, 8080, 8799, 3128\033[0m"
+echo -e "\033[1;31m-----> \033[1;37mSSH NOS IPS: \033[1;31m$ip, localhost, 127.0.0.1\033[0m"
+echo "                                           "
+echo "                                           "
+echo "                                           "
+clear
+echo "                                           "
+echo "
+echo " Por favor, aguarde até o fim da instalação dos pacotes"
+echo "                                            "
+echo "                                            "
+echo "                                            "
+clear
+dnf update -y
+dnf install wget -y
+dnf install squid -y
+dnf install nano -y
+dnf install python -y
+clear
+rm /etc/squid/squid.conf
+cd /etc/squid
+wget https://pastebin.com/raw/124vStHp -O squid.conf && wget http://pastebin.com/raw/UFJ7RbDN -O payload.txt
+service squid restart
+echo "                                              "
+echo "                                              "
+echo "                                              "
+echo " Crie um usuário teste com o seguinte comando "
+echo "                                              "
+echo " [ possui acesso ao shell ]: useradd ( nome de usuário ) "
+echo "                                                         "
+echo " [ sem acesso ao shell ]: useradd -M -s /bin/false ( nome de usuário ) "
+echo "                                              "
+echo " [ sem acesso ao shell e com data de expiração ]: useradd -e 2017-04-29 -M -s /bin/false (nome de usuario) "
