b2bbc7aa758489786b5643ab3fbfcbf71dea9739
mundodoug <dougeproxy@gmail.com>
2017-04-28 21:45:34 -0300

    Update mundodoug.sh

--- mundodoug.sh
+++ mundodoug.sh
@@ -5,6 +5,7 @@ echo "=========================================="
 echo "================ Feito por @Dougssh ======"
 echo "=========================================="
 echo "          "
+sleep 1
 echo -e "\033[0;32mPara a instalação ser correta é preciso o ip.
 Digite o ip !\033[0m"
 read -p ": " ip
@@ -17,7 +18,9 @@ echo " "
 echo -e "\033[1;33m-----> \033[1;33mPor favor, aguarde até o fim da instalação dos pacotes[0m"
 echo ""
 echo "                                            "
-echo "                                            "
+echo "  "
+sleep 1
+echo " "
 dnf update -y
 dnf install wget -y
 dnf install squid -y
@@ -46,9 +49,27 @@ wget https://raw.githubusercontent.com/K1R170/openvivo/master/openvivo.sh
 echo " "
 cd 
 clear
-wget https://raw.githubusercontent.com/mundodoug/squid.sh/master/firewall.sh
-chmod u+x firewall.sh
-./firewall.sh
+echo Bloqueando torrent.......
+sleep 1
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 6881:6889 -j DNAT --to-dest $ip
+iptables -A FORWARD -p tcp -i eth0 --dport 6881:6889 -d $ip -j REJECT
+iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
+iptables -A OUTPUT -p udp --dport 6881:6889 -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+
+echo Firewall configurado...
+sleep 1
+
 echo " "
 cd
 clear 
