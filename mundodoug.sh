134cfe133b465499906660b1a810a51a353a092c
mundodoug <dougeproxy@gmail.com>
2017-04-29 12:31:32 -0300

    Update mundodoug.sh

--- mundodoug.sh
+++ mundodoug.sh
@@ -4,13 +4,18 @@ echo "=== Script Oficial do Canal @MundoDoug ==="
 echo "=========================================="
 echo "================ Feito por @Dougssh ======"
 echo "=========================================="
-echo "==================== Apenas para Fedora =="
+echo "==================== FEDORA =============="
+echo "============ DEBIAN======================="
+echo "== UBUNTU == 14 ==== & ===== 16 =========="
 echo "=========================================="
+echo -e "\033[0;32mPara a instalação ser correta é preciso o ip.
+Digite o ip !\033[0m"
+read -p "Qual seu IP?: " IP
 echo " "
-echo -e "\033[0;32m-----> \033[0;32mSeu sistema operacional: Fedora\033[0;32m"
+echo -e "\033[0;32m-----> \033[0;32mSeu sistema operacional:\033[1;31m $(cat /etc/so)"
 echo -e "\033[0;32m-----> \033[0;32mCanal:\033[0;32m @MundoDoug"
 echo -e "\033[0;32m-----> \033[0;32mSQUID NAS PORTAS:\033[1;33m 80, 8080, 8799, 3128\033[0m"
-echo -e "\033[0;32m-----> \033[0;32mSSH NOS IPS: \033[1;33m localhost, 127.0.0.1\033[0m"
+echo -e "\033[0;32m-----> \033[0;32mSSH NOS IPS: \033[1;31m$IP, localhost, 127.0.0.1\033[0m"
 echo " "
 echo -e "\033[1;33m-----> \033[1;33mPor favor, aguarde até o fim da instalação dos pacotes[0m"
 echo ""
@@ -18,6 +23,8 @@ echo "                                            "
 echo "  "
 sleep 1
 echo " "
+if cat /etc/so |grep -i fedora 1> /dev/null 2> /dev/null ; then
+echo -e "\033[01;37mConfigurando, Aguarde...\033[0m"
 dnf update -y
 dnf install wget -y
 dnf install squid -y
@@ -27,13 +34,9 @@ dnf install iptables -y
 rm /etc/squid/squid.conf
 clear
 cd /etc/squid
-echo -e "\033[0;32mPara a instalação ser correta é preciso o ip.
-Digite o ip !\033[0m"
-read -p "Qual seu IP?: " IP
-echo " Aguarde Configurando Squid"
+echo " Aguarde Configurando o Squid"
 sleep 5
 echo " 
-#
 # Melhor configuração para o seu squid By @Dougssh
 #
 # Canal: https://t.me/MundoDoug
@@ -61,7 +64,6 @@ visible_hostname @Dougssh
 http_access allow url1
 http_access allow url2
 http_access allow ip
-http_access allow iplocal
  
 # Squid normalmente escuta a porta:
 http_port 80
@@ -90,7 +92,206 @@ via off
 #Patrocinado pelo @MundoDoug
 # O canal mais amado e odiado do telegram" > /etc/squid/squid.conf
 clear
-echo" "
+if cat /etc/so |grep -i debian 1> /dev/null 2> /dev/null ; then
+echo -e "\033[01;37mConfigurando, Aguarde...\033[0m"
+apt-get update 
+apt-get install -y squid3 
+rm /etc/squid/squid3.conf
+clear
+cd /etc/squid3
+echo " "
+echo " Aguarde Configurando Squid"
+sleep 5
+echo " 
+# Melhor configuração para o seu squid By @Dougssh
+#
+# Canal: https://t.me/MundoDoug
+#
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adaptar para listar suas redes IP (internas) de onde a navegação
+# deveria ser permitido
+ 
+acl localnet src 0.0.0.1-0.255.255.255  # RFC 1122 "esta" rede (LAN)
+acl localnet src 10.0.0.0/8             # RFC 1918 rede local privada (LAN)
+acl localnet src 100.64.0.0/10          # RFC 6598 espaço de endereço compartilhado (CGN)
+ 
+acl url1 dstdomain -i sdp.vivo.com.br
+acl url2 dstdomain -i ddivulga.com.br
+acl ip dstdomain -i $IP
+#
+# INSIRA SUA (S) PRÓPRIA REGRA (S) AQUI PARA PERMITIR O ACESSO DOS SEUS CLIENTES:
+#
+visible_hostname @Dougssh
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adapte localnet na seção ACL para listar suas redes IP (internas)
+# De onde a navegação deve ser permitida
+http_access allow url1
+http_access allow url2
+http_access allow ip
+ 
+# Squid normalmente escuta a porta:
+http_port 80
+http_port 8080
+http_port 3128
+http_port 8799
+ 
+# descomente e ajuste o seguinte para adicionar um diretório de cache de disco
+#cache_dir ufs /var/spool/squid 100 16 256
+ 
+# Deixe coredumps no primeiro diretório de cache
+#coredump_dir /var/spool/squid3
+ 
+#
+# Adicione qualquer uma das suas próprias entradas refresh_pattern acima dessas.
+#
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+ 
+#Torne-se anônimo
+forwarded_for off
+via off
+ 
+#Patrocinado pelo @MundoDoug
+# O canal mais amado e odiado do telegram" > /etc/squid3/squid.conf
+clear
+if cat /etc/so |grep -i ubuntu 1> /dev/null 2> /dev/null ; then
+echo -e "\033[01;37mConfigurando, Aguarde...\033[0m"
+apt-get update 
+apt-get install -y squid3 
+rm /etc/squid/squid3.conf
+clear
+cd /etc/squid3
+echo " "
+echo " Aguarde Configurando Squid"
+sleep 5
+echo " 
+# Melhor configuração para o seu squid By @Dougssh
+#
+# Canal: https://t.me/MundoDoug
+#
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adaptar para listar suas redes IP (internas) de onde a navegação
+# deveria ser permitido
+ 
+acl localnet src 0.0.0.1-0.255.255.255  # RFC 1122 "esta" rede (LAN)
+acl localnet src 10.0.0.0/8             # RFC 1918 rede local privada (LAN)
+acl localnet src 100.64.0.0/10          # RFC 6598 espaço de endereço compartilhado (CGN)
+ 
+acl url1 dstdomain -i sdp.vivo.com.br
+acl url2 dstdomain -i ddivulga.com.br
+acl ip dstdomain -i $IP
+#
+# INSIRA SUA (S) PRÓPRIA REGRA (S) AQUI PARA PERMITIR O ACESSO DOS SEUS CLIENTES:
+#
+visible_hostname @Dougssh
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adapte localnet na seção ACL para listar suas redes IP (internas)
+# De onde a navegação deve ser permitida
+http_access allow url1
+http_access allow url2
+http_access allow ip
+ 
+# Squid normalmente escuta a porta:
+http_port 80
+http_port 8080
+http_port 3128
+http_port 8799
+ 
+# descomente e ajuste o seguinte para adicionar um diretório de cache de disco
+#cache_dir ufs /var/spool/squid 100 16 256
+ 
+# Deixe coredumps no primeiro diretório de cache
+#coredump_dir /var/spool/squid3
+ 
+#
+# Adicione qualquer uma das suas próprias entradas refresh_pattern acima dessas.
+#
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+ 
+#Torne-se anônimo
+forwarded_for off
+via off
+ 
+#Patrocinado pelo @MundoDoug
+# O canal mais amado e odiado do telegram" > /etc/squid3/squid.conf
+clear
+echo " "
+if cat /etc/so |grep -i ubuntu |grep 16 1> /dev/null 2> /dev/null ; then
+echo -e "\033[01;37mConfigurando, Aguarde...\033[0m"
+apt-get update 
+apt-get install -y squid 
+rm /etc/squid/squid.conf
+clear
+cd /etc/squid3
+echo " "
+echo " Aguarde Configurando Squid"
+sleep 5
+echo " 
+# Melhor configuração para o seu squid By @Dougssh
+#
+# Canal: https://t.me/MundoDoug
+#
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adaptar para listar suas redes IP (internas) de onde a navegação
+# deveria ser permitido
+ 
+acl localnet src 0.0.0.1-0.255.255.255  # RFC 1122 "esta" rede (LAN)
+acl localnet src 10.0.0.0/8             # RFC 1918 rede local privada (LAN)
+acl localnet src 100.64.0.0/10          # RFC 6598 espaço de endereço compartilhado (CGN)
+ 
+acl url1 dstdomain -i sdp.vivo.com.br
+acl url2 dstdomain -i ddivulga.com.br
+acl ip dstdomain -i $IP
+#
+# INSIRA SUA (S) PRÓPRIA REGRA (S) AQUI PARA PERMITIR O ACESSO DOS SEUS CLIENTES:
+#
+visible_hostname @Dougssh
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adapte localnet na seção ACL para listar suas redes IP (internas)
+# De onde a navegação deve ser permitida
+http_access allow url1
+http_access allow url2
+http_access allow ip
+ 
+# Squid normalmente escuta a porta:
+http_port 80
+http_port 8080
+http_port 3128
+http_port 8799
+ 
+# descomente e ajuste o seguinte para adicionar um diretório de cache de disco
+#cache_dir ufs /var/spool/squid 100 16 256
+ 
+# Deixe coredumps no primeiro diretório de cache
+#coredump_dir /var/spool/squid3
+ 
+#
+# Adicione qualquer uma das suas próprias entradas refresh_pattern acima dessas.
+#
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+ 
+#Torne-se anônimo
+forwarded_for off
+via off
+ 
+#Patrocinado pelo @MundoDoug
+# O canal mais amado e odiado do telegram" > /etc/squid/squid.conf
+clear
+echo " "
 echo "Aguarde mais um momento para criar um arquivo openvpn"
 echo " "
 cd
