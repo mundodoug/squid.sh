6fb0f17b7eb98504a69e08bab86fa6aff40b94fb
mundodoug <dougeproxy@gmail.com>
2017-04-29 23:10:49 -0300

    Update mundodoug.sh

--- mundodoug.sh
+++ mundodoug.sh
@@ -25,7 +25,7 @@ echo "    "
 echo "  "
 sleep 1
 echo " "
-if cat /etc/so |grep -i fedora 1> /dev/null 2> /dev/null ; then
+if cat /etc/so |grep -i Fedora 1> /dev/null 2> /dev/null ; then
 echo -e "\033[1;37mConfigurando Fedora, Aguarde...\033[0m"
 sleep 1
 dnf update -y 1> /dev/null 2> /dev/null
