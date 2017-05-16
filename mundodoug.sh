4c1f9f6aefdc8e9cf539fd16e1aefd6d200a51ff
mundodoug <dougeproxy@gmail.com>
2017-05-16 18:57:33 -0300

    Update mundodoug.sh

--- mundodoug.sh
+++ mundodoug.sh
@@ -258,16 +258,116 @@ echo -e "\033[1;33mt.me/MundoDoug\033[1;33m"
 PS1="\[\e[0;32m\][\t] => \[\e[0m\]"
 exit 0
 fi
-if cat /etc/so |grep -i ubuntu 14 1> /dev/null 2> /dev/null ; then
-echo -e "\033[01;37mConfigurando Ubuntu, Aguarde...\033[01;37m"
-echo " "
+if cat /etc/so |grep -i ubuntu |grep 16 1> /dev/null 2> /dev/null ; then
+echo -e "\033[1;37mConfigurando, Aguarde...\033[0m"
+apt-get update 1> /dev/null 2> /dev/null
+apt-get install -y squid3 1> /dev/null 2> /dev/null
 
-apt-get update 
-apt-get install -y squid3
-clear
-cd /etc/squid3
-rm /etc/squid3/squid.conf
+echo " 
+# Melhor configuração para o seu squid By @Dougssh
+#
+# Canal: https://t.me/MundoDoug
+#
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adaptar para listar suas redes IP (internas) de onde a navegação
+# deveria ser permitido
+ 
+acl localnet src 0.0.0.1-0.255.255.255  # RFC 1122 "esta" rede (LAN)
+acl localnet src 10.0.0.0/8             # RFC 1918 rede local privada (LAN)
+acl localnet src 100.64.0.0/10          # RFC 6598 espaço de endereço compartilhado (CGN)
+ 
+acl url1 dstdomain -i sdp.vivo.com.br
+acl url2 dstdomain -i ddivulga.com.br
+acl ip dstdomain -i $IP
+#
+# INSIRA SUA (S) PRÓPRIA REGRA (S) AQUI PARA PERMITIR O ACESSO DOS SEUS CLIENTES:
+#
+visible_hostname @Dougssh
+ 
+# Exemplo de regra que permite o acesso de suas redes locais.
+# Adapte localnet na seção ACL para listar suas redes IP (internas)
+# De onde a navegação deve ser permitida
+http_access allow url1
+http_access allow url2
+http_access allow ip
+ 
+# Squid normalmente escuta a porta:
+http_port 80
+http_port 8080
+http_port 3128
+http_port 8799
+ 
+# descomente e ajuste o seguinte para adicionar um diretório de cache de disco
+#cache_dir ufs /var/spool/squid 100 16 256
+ 
+# Deixe coredumps no primeiro diretório de cache
+#coredump_dir /var/spool/squid3
+ 
+#
+# Adicione qualquer uma das suas próprias entradas refresh_pattern acima dessas.
+#
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+#refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+ 
+#Torne-se anônimo
+forwarded_for off
+via off
+ 
+#Patrocinado pelo @MundoDoug
+# O canal mais amado e odiado do telegram" > /etc/squid3/squid.conf
+sleep 1
+service squid3 restart
 echo " "
+echo Aguarde.....
+cd
+echo ""
+wget https://raw.githubusercontent.com/mundodoug/squid.sh/master/opendoug.sh
+chmod u+x opendoug.sh
+echo " "
+cd
+echo " "
+wget https://raw.githubusercontent.com/K1R170/openvivo/master/openvivo.sh
+echo " "
+cd 
+sleep 3
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 6881:6889 -j DNAT --to-dest 127.0.0.1
+iptables -A FORWARD -p tcp -i eth0 --dport 6881:6889 -d 127.0.0.1 -j REJECT
+iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
+iptables -A OUTPUT -p udp --dport 6881:6889 -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+echo " "
+clear
+sleep 1
+echo -e "\033[0;32mCriado por @Dougssh do canal @MundoDoug \033[0;32m"
+echo " "
+echo -e "\033[01;31mPara utilizar o Openvpn Unilogin, utilize o comando:\033[01;31m" 
+echo -e "\033[1;33m bash openvivo.sh\033[1;33m"
+echo " "
+echo -e "\033[01;31mPara utilizar o Openvpn Multilogin, use o comando:\033[01;31m"
+echo -e "\033[1;33m./opendoug.sh\033[1;33m"
+echo " "
+echo -e "\033[01;31mEntre no meu canal e seja bem vindo:\033[01;31m"
+echo -e "\033[1;33mt.me/MundoDoug\033[1;33m"
+PS1="\[\e[0;32m\][\t] => \[\e[0m\]"
+exit 0
+fi
+if cat /etc/so |grep -i ubuntu |grep 17 1> /dev/null 2> /dev/null ; then
+echo -e "\033[1;37mConfigurando, Aguarde...\033[0m"
+apt-get update 1> /dev/null 2> /dev/null
+apt-get install -y squid3 1> /dev/null 2> /dev/null
 
 echo " 
 # Melhor configuração para o seu squid By @Dougssh
@@ -371,7 +471,7 @@ PS1="\[\e[0;32m\][\t] => \[\e[0m\]"
 exit 0
 fi
 if 
-cat /etc/so |grep -i Ubuntu 16.04.2 LTS 1> /dev/null 2> /dev/null 
+cat /etc/so |grep -i Ubuntu LTS 1> /dev/null 2> /dev/null 
 then
 echo -e "\033[1;37mConfigurando Ubuntu 16, Aguarde...\033[0m"
 echo " "
